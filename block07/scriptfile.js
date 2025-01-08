281c8101c3dcbd8d3da974bb3187921cb6001455
Jenn Milano <jmilano0354@gmail.com>
2025-01-08 15:11:07 -0500

    inital commit

---
+++ block07/scriptfile.js
@@ -0,0 +1,25 @@
+//pseudocode: How I will solve this exercise
+//Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+//assign 3 variables
+//each variable must equal one of the three codes in the combination
+//add comments throughout
+//create a dialog box that displays the vault codes and the text, or create a pop up dialog
+
+//create dialog with an alert
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+
+
+//create first variable using a const variable to equal 10
+const first = 5 + 5;
+
+//create second variable using a const variable to equal 40
+const second = 41 - 1;
+
+//create third variable using a const variable to equal 39
+const third = 1 * 39;
+
+//show combo in alert
+alert("Vault Combo is " + first + " - " + second + " - " + third)
+
+//create fun success
+alert("YOU'RE IN")
