01ff28a0e05bbbc85f2c7c1f9ca1dc6368bc57bb
Jenn Milano <jmilano0354@gmail.com>
2025-03-18 19:16:05 -0400

    updated line 7

--- puppy/workshop/src/main.jsx
+++ puppy/workshop/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import {Provider} from "react-redux";
-import store from "./app/store.js";
+import {store} from "./app/store.js";
 
 // React is for defining components and ReactDOM is for rendering them in the DOM
 
