650e7bc34686ae8865443bc71adc7b4325dbb755
Jenn Milano <jmilano0354@gmail.com>
2025-05-06 13:45:07 -0400

    initial setup created products page and template for storeApi. products page rendering successfully in front end

---
+++ Capstone/Capstone/src/App.jsx
@@ -0,0 +1,21 @@
+import './index.css'
+import { Routes, Route } from 'react-router-dom'
+import Products from './components/Products'
+
+//import AllPlayers from "./features/AllPlayers";
+//import MainRoute from './features/MainRoute'
+//import NewPlayerForm from './features/NewPlayerForm'
+
+// Define the App component
+function App() {
+  // This component renders the Players component inside a div
+  // This div has a class of 'App', which we could use for styling
+  return (
+    <Routes>
+      <Route path="/" element={<Products />} />
+    </Routes>
+  );
+}
+
+// Export the App component as the default export
+export default App;
---
+++ Capstone/Capstone/src/components/Products.jsx
@@ -0,0 +1,11 @@
+//import { useFetchProductsQuery } from "../store/apiSlice";
+//import { storeApi } from "../api/storeApi";
+
+export default function Products() {
+
+    return (
+        <div>
+            <h1>Products</h1>
+        </div>
+    )
+};
