52401570b47ae2ca0feeeb2a6ba746bd4854dc0e
Jenn Milano <jmilano0354@gmail.com>
2025-05-08 18:58:20 -0400

    updated image styling

--- Capstone/Capstone/src/App.jsx
+++ Capstone/Capstone/src/App.jsx
@@ -1,5 +1,5 @@
 import './index.css'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Products from './components/Products'
 import React from 'react'
 import SingleProduct from './components/SingleProduct'
@@ -7,27 +7,34 @@ import Login from './components/login'
 import Navigation from './components/Navigation'
 import { useSelector } from 'react-redux'
 import { selectToken } from './redux/authSlice'
+import Cart from './components/Cart'
+import Register from './components/Register'
+import CreateProduct from './components/CreateProduct'
 
 function App() {
   const token = useSelector(selectToken);
 
   return (
-    <div className="app">
-      <header>
-        <h1>NFL App</h1>
-        <Navigation />
-      </header>
+    <Router>
+      <div className="app">
+        <header>
+          <h1>NFL App</h1>
+          <Navigation />
+        </header>
 
-      <main>
-        <section>
+        <main>
           <Routes>
             <Route path="/" element={<Products />} />
-            <Route path="/products/:product_id" element={<SingleProduct />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<SingleProduct />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/create-product" element={<CreateProduct />} />
           </Routes>
-        </section>
-      </main>
-    </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
