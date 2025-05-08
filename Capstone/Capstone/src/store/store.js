80e5355e23df4f9e09aec20edcf2492378b42cf0
Jenn Milano <jmilano0354@gmail.com>
2025-05-08 17:31:18 -0400

    nav updates

--- Capstone/Capstone/src/store/store.js
+++ Capstone/Capstone/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "../api/API";
+import authReducer from "../redux/authSlice";
 
 export const store = configureStore({
     reducer: {
         [api.reducerPath]: api.reducer,
+        auth: authReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
     
