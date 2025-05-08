79a7645a571ce5b8ef529a772a7c46f471e871e4
Jenn Milano <jmilano0354@gmail.com>
2025-05-08 17:31:01 -0400

    linked navigation

--- Capstone/Capstone/src/App.jsx
+++ Capstone/Capstone/src/App.jsx
@@ -3,18 +3,32 @@ import { Routes, Route } from 'react-router-dom'
 import Products from './components/Products'
 import React from 'react'
 import SingleProduct from './components/SingleProduct'
+import Login from './components/login'
+import Navigation from './components/Navigation'
+import { useSelector } from 'react-redux'
+import { selectToken } from './redux/authSlice'
 
 function App() {
+  const token = useSelector(selectToken);
 
   return (
-    <section>
-    <Routes>
-      <Route path="/" element={<Products />} />
-      <Route path="/products/:product_id" element={<SingleProduct />} />
-    </Routes>
-    </section>
+    <div className="app">
+      <header>
+        <h1>NFL App</h1>
+        <Navigation />
+      </header>
+
+      <main>
+        <section>
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/products/:product_id" element={<SingleProduct />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </main>
+    </div>
   );
 }
-
 
 export default App;
